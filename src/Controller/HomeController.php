03a1bcd90888f0d345b8b71f5f5ef457d1f0b89e
mohamed <mohamedguiliano@hotmail.fr>
2025-02-23 17:33:41 +0100

    Premier commit - Ajout du projet Symfony ZenCAR

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route('/services', name: 'app_services')]
+    public function services(): Response
+    {
+        return $this->render('services.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+    }
+
